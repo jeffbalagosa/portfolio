87762ed7771a822658d07abc7378fe398424da3a
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2022-05-27 07:18:03 -0700

    Updated h2 for projects on index.html.  Updated ids for header section in index.html.  Created main-script.js to add interactivity. Adjusted header p for large screen media in style.css.

---
+++ main-script.js
@@ -0,0 +1,24 @@
+const tagLineArr = [
+  "Is A Brazilian Jiu Jitsu Blackbelt (Est. 2020)",
+  "Has Trained With UFC Champions",
+  "Loves His Wife And 2 Kids",
+  "Spends Too Much Time On Video Games",
+  "Is Passionate About Technology And Engineering",
+  "Is A Martial Arts Instructor",
+  "Is A Disciple Of Stoic Philosophy",
+  "Drinks Too Much Caffiene",
+  "Spent Most Of His Adult Life Working In Tech",
+  "Always Needs More Sleep",
+  "Meditates Daily",
+  "Believes Discipline Is True Freedom",
+];
+
+const headerContainer = document.getElementById("header-container");
+const headerTagLine = document.getElementById("header-tagline");
+console.log(chance.pickone(tagLineArr));
+
+headerContainer.onclick = function () {
+  let tagLine = chance.pickone(tagLineArr);
+  console.log(tagLine);
+  headerTagLine.innerHTML = tagLine;
+};
